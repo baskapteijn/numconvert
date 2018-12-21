469ee3cc7e431af9ef92de5d5f6e9869bf55f790
Bas Kapteijn <bask@ghs.com>
2018-12-21 11:59:09 +0100

    Added simple functional_test.sh script to compile numconvert and let the functional executable run it

---
+++ test/functional/functional_test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+exit_on_error()
+{
+    arg1=$1
+
+    echo "Failed to execute with error "$arg1". Exiting..."
+    exit $arg1
+}
+
+# Compile numconvert
+{
+    gcc -O3 ../../src/main.c -o numconvert
+} &> /dev/null
+rc=$?; if [[ $rc != 0 ]]; then exit_on_error $rc; fi
+
+# Compile the functional test program
+{
+    gcc -D_BSD_SOURCE -O3 main.c -o functional
+} &> /dev/null
+rc=$?; if [[ $rc != 0 ]]; then exit_on_error $rc; fi
+
+./functional "numconvert 0x12"
+rc=$?; if [[ $rc != 0 ]]; then exit_on_error $rc; fi
+
+echo "Script completed."
+
+exit 0
