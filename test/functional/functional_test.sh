d8c6f01919d470a2a6cc7dcc0d20ca5c92485701
Bas Kapteijn <bask@ghs.com>
2018-12-21 16:00:04 +0100

    Updated Travis CI script

--- test/functional/functional_test.sh
+++ test/functional/functional_test.sh
@@ -26,21 +26,21 @@ rc=$?; if [[ $rc != 0 ]]; then exit_on_error $rc; fi
 
 
 # Perform a number of decimal input tests
-./functional "1" "./numconvert" "0" "0" "stdout/1.txt"                       #lower decimal boundary
+./functional "1" "./numconvert" "0" "0" "stdout/1.txt"                          #lower decimal boundary
 rc=$?; if [[ $rc != 0 ]]; then EXIT_FAILURE=$rc; fi
 
-./functional "2" "./numconvert" "18446744073709551615" "0" "stdout/2.txt"    #upper decimal boundary
+./functional "2" "./numconvert" "18446744073709551615" "0" "stdout/2.txt"       #upper decimal boundary
 rc=$?; if [[ $rc != 0 ]]; then EXIT_FAILURE=$rc; fi
 
-./functional "3" "./numconvert" "-1" "255" "stdout/3.txt"                    #invalid lower decimal boundary
+./functional "3" "./numconvert" "-1" "255" "stdout/3.txt"                       #invalid lower decimal boundary
 rc=$?; if [[ $rc != 0 ]]; then EXIT_FAILURE=$rc; fi
 
-./functional "4" "./numconvert" "18446744073709551616" "255" "stdout/4.txt"  #invalid upper decimal boundary
+./functional "4" "./numconvert" "18446744073709551616" "255" "stdout/4.txt"     #invalid upper decimal boundary
 rc=$?; if [[ $rc != 0 ]]; then EXIT_FAILURE=$rc; fi
 
 
 # Perform a number of binary input tests
-./functional "5" "./numconvert" "0b" "0" "stdout/5.txt"                       #lower binary boundary
+./functional "5" "./numconvert" "0b" "0" "stdout/5.txt"                         #lower binary boundary
 rc=$?; if [[ $rc != 0 ]]; then EXIT_FAILURE=$rc; fi
 
 ./functional "6" "./numconvert" "1111111111111111111111111111111111111111111111111111111111111111b" "0" "stdout/6.txt"       #upper binary boundary
@@ -51,22 +51,22 @@ rc=$?; if [[ $rc != 0 ]]; then EXIT_FAILURE=$rc; fi
 
 
 # Perform a number of hexadecimal input tests
-./functional "8" "./numconvert" "0x0" "0" "stdout/8.txt"                     #lower hexadecimal boundary
+./functional "8" "./numconvert" "0x0" "0" "stdout/8.txt"                        #lower hexadecimal boundary
 rc=$?; if [[ $rc != 0 ]]; then EXIT_FAILURE=$rc; fi
 
-./functional "9" "./numconvert" "0h" "0" "stdout/9.txt"                      #lower hexadecimal boundary
+./functional "9" "./numconvert" "0h" "0" "stdout/9.txt"                         #lower hexadecimal boundary
 rc=$?; if [[ $rc != 0 ]]; then EXIT_FAILURE=$rc; fi
 
-./functional "10" "./numconvert" "0xffffffffffffffff" "0" "stdout/10.txt"    #upper hexadecimal boundary
+./functional "10" "./numconvert" "0xffffffffffffffff" "0" "stdout/10.txt"       #upper hexadecimal boundary
 rc=$?; if [[ $rc != 0 ]]; then EXIT_FAILURE=$rc; fi
 
-./functional "11" "./numconvert" "ffffffffffffffffh" "0" "stdout/11.txt"     #upper hexadecimal boundary
+./functional "11" "./numconvert" "ffffffffffffffffh" "0" "stdout/11.txt"        #upper hexadecimal boundary
 rc=$?; if [[ $rc != 0 ]]; then EXIT_FAILURE=$rc; fi
 
-./functional "12" "./numconvert" "0x1ffffffffffffffff" "255" "stdout/12.txt"   #invalid upper hexadecimal boundary
+./functional "12" "./numconvert" "0x1ffffffffffffffff" "255" "stdout/12.txt"    #invalid upper hexadecimal boundary
 rc=$?; if [[ $rc != 0 ]]; then EXIT_FAILURE=$rc; fi
 
-./functional "13" "./numconvert" "1ffffffffffffffffh" "255" "stdout/13.txt"    #invalidupper hexadecimal boundary
+./functional "13" "./numconvert" "1ffffffffffffffffh" "255" "stdout/13.txt"     #invalidupper hexadecimal boundary
 rc=$?; if [[ $rc != 0 ]]; then EXIT_FAILURE=$rc; fi
 
 
